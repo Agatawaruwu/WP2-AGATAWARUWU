e0d2a7a62c57eaa406b8ccdd3774a7bed0e769f7
Agatawaruwu <agatabisnis@gmail.com>
2021-10-03 22:44:08 +0700

    first commit

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,9 +3,9 @@ class contoh1 extends CI_Controller {
     public function index()
     {
         echo"<h1>Perkenalkan</h1>";
-        echo"Nama Saya Reza Fahlevi <br>
-        Saya tinggal di daerah Bekasi <br>
-        Olahraga yang saya sukai adalah bulutangkis"; 
+        echo"Nama Saya Agata waruwu <br>
+        Saya tinggal di daerah kelapa gading jakarta utara <br>
+        Olahraga yang saya sukai adalah badminton"; 
     }
 }
 
